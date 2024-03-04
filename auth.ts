bdbd07250dd13d785118ab394a44be398f09d651
Tupitsyn Aleksandr <aleksandr.tupitcyn@mostgames.org>
2024-03-04 16:13:52 +0900

    Removed unnecessary files from the repository

--- auth.ts
+++ auth.ts
@@ -1,6 +1,9 @@
 import NextAuth from 'next-auth'
 import { authConfig } from './auth.config'
 import Credentials from 'next-auth/providers/credentials'
+import { sql } from '@vercel/postgres'
+import type { User } from '@/app/lib/definitions'
+import bcrypt from 'bcrypt'
 
 export const { auth, signIn, signOut } = NextAuth({
     ...authConfig,
